294535a7b6b73b82d82bb5056ec73a15c31644d2
palesl <patrick.leslie@anu.edu.au>
2024-04-17 10:00:43 +1000

    fix

--- R/10_modelling.R
+++ R/10_modelling.R
@@ -14,11 +14,17 @@ library(sjPlot)
 analysis_data <- readRDS("outputs/analysis_data.rds")
 
 # removing any NAs 
-
+# and any questions on notice
 future_focus <- analysis_data|>
-  select(person,Future,InGov,Party,yearsSince1972,month,day,
-         in_cohort,Age)|>
-  na.omit()
+    filter(!grepl("QUESTIONS ON NOTICE", oral_heading)) |>
+    select(person,Future,InGov,Party,
+           date,yearsSince1972,month,day,
+           in_cohort,Age,nchars)|>
+    na.omit() |>
+    group_by(person, InGov, Party, yearsSince1972, date,month, day,
+             in_cohort, Age) |>
+    summarize(Future = weighted.mean(Future, nchars),
+              nchars = sum(nchars))
 
 future_focus$InGov<-as.factor(as.numeric(future_focus$InGov))
 future_focus$Party<-as.factor(future_focus$Party)
@@ -27,6 +33,8 @@ future_focus$month<-as.factor(as.character(future_focus$month))
 future_focus$day<-as.factor(as.character(future_focus$day))
 
 future_focus$in_cohort<-as.factor(future_focus$in_cohort)
+
+future_focus$w8 <- future_focus$nchars / mean(future_focus$nchars)
 
 #debug
 
@@ -44,19 +52,27 @@ model<-bam(Future ~
              s(person, bs="re") + 
              # Political variables
              InGov + s(Party, bs="re") + 
+<<<<<<< HEAD
              # period
              s(yearsSince1972,bs="cr", k=20)+ day + month+
              # cohort
+=======
+             #period
+             s(yearsSince1972,bs="cr", k=20) + day + month+
+             #cohort
+>>>>>>> c2edabc25f8eb39998b349635a55d4451c07b574
              s(in_cohort, bs="re")+
              # age
-             s(Age,bs="cr", k=30),
+             s(Age, bs="cr", k=30),
            nthreads=6,
+           weights = future_focus$w8,
            family=betar(link="logit"),
-           knots=list(yearsSince1972=seq(0,47,length=20),
-                      Age=seq(23,77,length=30)),
+           ## knots=list(yearsSince1972=seq(0,47,length=20),
+           ##            Age=seq(23,77,length=30)),
            data = future_focus)
 toc()
 
+saveRDS(model, file = here::here("working",
+                                 "senate_tfs_bam_model.rds"))
 
-plot(model, all.terms = T)
 
